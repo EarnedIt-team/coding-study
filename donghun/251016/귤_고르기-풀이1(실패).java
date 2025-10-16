9de6adf7a561cfb6c129667d31176c0f11aad4a8
saaad99 <ehdgnstla@naver.com>
2025-10-16 10:39:04 +0900

    N개의 최소 공배수 커밋

--- donghun/251016/귤_고르기-풀이1(실패).java
+++ donghun/251016/귤_고르기-풀이1(실패).java
@@ -1,51 +1,50 @@
 // 풀이 1 - 실패
-// import java.util.*;
+import java.util.*;
 
-// class Solution {
-//     public int solution(int k, int[] tangerine) {
+class Solution {
+    public int solution(int k, int[] tangerine) {
         
-//         // [1, 2, 2, 3, 3, 4, 5, 5]
+        // [1, 2, 2, 3, 3, 4, 5, 5]
         
-//         // 귤 크기를 Map에 기록하고 value가 높은 값 순서로 정렬
-//         Map<Integer, Integer> map = new HashMap<>();
+        // 귤 크기를 Map에 기록하고 value가 높은 값 순서로 정렬
+        Map<Integer, Integer> map = new HashMap<>();
         
-//         for(int tang : tangerine) {
-//             map.put(tang, map.getOrDefault(tang, 0) + 1); // 해당 귤의 크기를 카운트
-//         }
+        for(int tang : tangerine) {
+            map.put(tang, map.getOrDefault(tang, 0) + 1); // 해당 귤의 크기를 카운트
+        }
         
-//         // map에 저장된 값을 저장할 리스트
-//         List<int[]> list = new ArrayList<>();
+        // map에 저장된 값을 저장할 리스트
+        List<int[]> list = new ArrayList<>();
         
-//         // 리스트에 키-값 저장
-//         for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-//             list.add(new int[]{entry.getKey(), entry.getValue()});
-//         }
+        // 리스트에 키-값 저장
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            list.add(new int[]{entry.getKey(), entry.getValue()});
+        }
         
-//         // 정렬
-//         list.sort((a,b) -> b[1] - a[1]);
+        // 정렬
+        list.sort((a,b) -> b[1] - a[1]);
         
-        
-//        int answer = 0;
+       int answer = 0;
 
-//         int sum = 0;
-//         for (int[] arr : list) {
-//             sum+= arr[1];
+        int sum = 0;
+        for (int[] arr : list) {
+            sum+= arr[1];
             
-//             if(sum <= k) {
-//                 answer++;
-//             } else if(arr[1] > k) {
-//                 answer++;
-//                 break;
-//             } else {
-//                 break;
-//             }
+            if(sum <= k) {
+                answer++;
+            } else if(arr[1] > k) {
+                answer++;
+                break;
+            } else {
+                break;
+            }
             
-//             // System.out.println("[" + arr[0] + "," + arr[1] + "]");
+            // System.out.println("[" + arr[0] + "," + arr[1] + "]");
             
-//         }
+        }
         
         
         
-//         return answer;
-//     }
-// }
+        return answer;
+    }
+}
