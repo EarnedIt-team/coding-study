ad45b41538a776db4717cf45e7321268cf034653
saaad99 <ehdgnstla@naver.com>
2025-10-24 09:54:10 +0900

    피로도 풀이 커밋

---
+++ donghun/251024/피로도_풀이1.java
@@ -0,0 +1,40 @@
+// 풀이 1. 성공 (depth 값을 리스트에 저장하고 내림차순 정렬 후 첫 번째 값 리턴)
+import java.util.*;
+
+class Solution {
+    
+    List<Integer> arr = new ArrayList<>();
+    int depth = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        
+        // dfs 알고리즘으로 가장 깊이가 큰 값을 리턴하는 식으로 ㄱㄱ
+        dfs(k, dungeons, visited, depth);
+        
+        // 내림차순 정렬
+        Collections.sort(arr, Collections.reverseOrder());
+        
+        int answer = arr.get(0);
+        return answer;
+    }
+    
+    void dfs(int k, int[][] dungeons, boolean[] visited, int depth) {
+        arr.add(depth);
+        
+        for(int i = 0; i < dungeons.length; i++) {
+            // 아직 방문하지 않았고, 최소 필요 피로도 조건이 맞다면
+            if(!visited[i] && dungeons[i][0] <= k) {
+                
+                // 해당 던전 방문 표시
+                visited[i] = true;
+                
+                dfs(k - dungeons[i][1], dungeons, visited, depth + 1);
+                
+                // 최대 깊이까지 방문 후 false 처리
+                visited[i] = false;
+            } 
+        }
+    }
+}
+
