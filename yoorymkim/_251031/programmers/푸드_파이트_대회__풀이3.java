fb0c3544cd1758e20ab9622f2f548cfa38e9e51d
yoorym-kim <twilight99me@gmail.com>
2025-10-31 10:20:20 +0900

    1031 : 푸드 파이트 대회 풀이3(통과o)

---
+++ yoorymkim/_251031/programmers/푸드_파이트_대회__풀이3.java
@@ -0,0 +1,23 @@
+package _251031.programmers;
+
+import java.util.*;
+
+public class 푸드_파이트_대회__풀이3 {
+
+    class Solution {
+        public String solution(int[] food) {
+            // food index:칼로리(0은 물), val:개수(/2하여 대칭배치사용)
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i < food.length; i++) { // 물(0)제외
+                for (int j = 0; j < food[i]/2; j++) {
+                    sb.append(i);
+                }
+            }
+
+            StringBuilder rev = new StringBuilder(sb).reverse();
+
+            return sb.append(0).append(rev).toString();
+        }
+    }
+}
