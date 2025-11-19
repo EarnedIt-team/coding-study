9cf9b1e8354f2dfda56405dfd45b32ffb862de6f
yoorym-kim <twilight99me@gmail.com>
2025-11-19 10:56:14 +0900

    1119 : 풀이 2에서 주석 정리 + 맵 필요없는거 빼기

--- yoorymkim/_251119/programmers/프로세스_풀이2_최적화ver.java
+++ yoorymkim/_251119/programmers/프로세스_풀이2_최적화ver.java
@@ -7,28 +7,24 @@ public class 프로세스_풀이2_최적화ver {
     class Solution {
 
         public int solution(int[] priorities, int location) {
-            // [1, 1, 9, 1, 1, 1] 이므로 [C, D, E, F, A, B] 순
-            // 숫자가 클 수록 우선순위가 높다
 
-            HashMap<Integer, Integer> map = new HashMap<>(); // key:ind, val:priority
             Queue<Integer> q = new LinkedList<>(); // ind 넣기
-
             int[] count = new int[10]; // ind:priority, val:개수
 
             for (int i = 0; i < priorities.length; i++) {
-                map.put(i, priorities[i]);
                 q.offer(i);
-                count[priorities[i]]++; // 등장 횟수 증가
+                count[priorities[i]]++;
             }
 
             int answer = 0;
+
             while (!q.isEmpty()) {
                 int i = q.poll();
-                int p = map.get(i);
+                int p = priorities[i];
 
-                if (isThereHigherPriorityFast(p, count)) q.offer(i);
-                else {
-                    map.remove(i);
+                if (isThereHigherPriorityFast(p, count)) {
+                    q.offer(i);
+                } else {
                     count[p]--;
                     answer++;
                     if (i == location) return answer;
