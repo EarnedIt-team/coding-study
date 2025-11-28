8a5e811b919779cdd8f92d5c5f12d9186e9749cc
ParkJunYoung <j77777y@naver.com>
2025-11-28 10:47:12 +0900

    #127 문제1 풀이 수정

--- parkj/_251128/택배상자.java
+++ parkj/_251128/택배상자.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        int answer = 0;
+        int answer = 0;   
         int nextBox = 1; // 다음에 오는 택배순서
         Stack<Integer> stack = new Stack<>();
         
         // order대로 물량 처리하기
-        for (int i = 0; i < order.length; i++) {
+        for (int item : order) {
             // order[i]가 나올 때까지 계속 보조 컨테이너에 추가
             while (nextBox <= order.length && 
-                   (stack.isEmpty() || stack.peek() != order[i])) {
+                   (stack.isEmpty() || stack.peek() != item)) {
                 stack.push(nextBox++); // 1, 2, 3, ...
             }
-
+          
             // 보조 컨테이너 앞에 order[i]값 이라면 트럭에 실음
-            if (!stack.isEmpty() && stack.peek() == order[i]) {
+            if (!stack.isEmpty() && stack.peek() == item) {
                 stack.pop();
                 answer++;
             } else {
